de61462c265636f8d5837b8b21daa8cf0a7ca6d4
Dom <163606370+DomHouricanTraining@users.noreply.github.com>
2024-05-05 21:25:34 +0100

    Autoscaling stuff

--- main.tf
+++ main.tf
@@ -36,22 +36,31 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.4.1"
+  # insert the 1 required variable here
+  
+  name = "blog"
+  min_size = 1
+  max_size = 2
 
-  subnet_id = module.blog_vpc.public_subnets[0]
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups      = [module.blog_sg.security_group_id]
 
-  tags = {
-    Name = "Learning Terraform, Matey"
-  }
+  image_id            = data.aws_ami.app_ami.id
+  instance_type       = var.instance_type
+
 }
 
-module "alb" {
-  source = "terraform-aws-modules/alb/aws"
+module "blog_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
   name    = "blog-alb"
+
+  load_balancer_type = "application"
 
   vpc_id          = module.blog_vpc.vpc_id
   subnets         = module.blog_vpc.public_subnets
